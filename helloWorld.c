5a7ced0d5994c8921175c8bd7acdcaf733ba8e32
atomhax <adammiller7@gmail.com>
2014-02-13 20:35:46 -0700

    Fixed the newb mistake that was made by Adam's poorly placed comment

--- helloWorld.c
+++ helloWorld.c
@@ -1,9 +1,10 @@
 /* Hello World program */
-/Comment from Adam Miller
+//Comment from Adam Miller
 #include<stdio.h>
 
 main() /* the main method */
 {
-    printf("Hello World"); /* This prints out Hello World*/
+    printf("Hello World"); 
+/* This prints out Hello World*/
 
 }
