0e284aa389f55491669e1fa5127a82f390568b4d
atomhax <adammiller7@gmail.com>
2014-02-23 11:41:21 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "common.h"
+
+int main (int argc, const char *argv[])
+{
+    FILE *source_file;
+    char source_name[MAX_FILE_NAME_LENGTH];
+    char date[DATE_STRING_LENGTH];
+    
+   /* Missing Code Here */
+    return 0;
+}
+FILE *init_lister(const char *name, char source_file_name[], char dte[])
+{
+    time_t timer;
+    FILE *file;
+    
+    /* Missing Code Here */
+    return file;
+}
+BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
+{
+    char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
+    char source_buffer[MAX_SOURCE_LINE_LENGTH];
+    static int line_number = 0;
+    
+    if (1) //This is missing a condition
+    {
+		/*  Missing Code Here */
+        return (TRUE);
+    }
+    else
+    {
+        return (FALSE);
+    }
+}
+
