789d5c010958cd91b7f37db13de3a338f9ab38fd
atomhax <adammiller7@gmail.com>
2014-02-23 16:39:03 -0700

    added some comments to main
    added print_line code
    
    added the date
    
    deleted unused comments
    
    added finsihing touches to print_buffer

--- main.c
+++ main.c
@@ -9,40 +9,37 @@ int main (int argc, const char *argv[])
     char firstline[80];
     BOOLEAN notEndOfFile;
 
-   /* Missing Code Here */ 
-   
-   //source_file=fopen(argv[1],"r");
-   //fgets(firstline,80,source_file);
-   //puts(firstline);
-
+  
    source_file=init_lister(argv[1],source_name,date);
-   
+   //sets up the connection to the file
+
    do{
 
    notEndOfFile=get_source_line(source_file,source_name,date);
+   //this is the main loop of the program, it runs till
+   //it hits an end of file that will give us the exit case
 
    }while(notEndOfFile==TRUE);
 
+fclose(source_file); //closes the file 
 
 return 0;
+
 }
+
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
 {
     time_t timer;
 
     FILE *file;
     
-    /* Missing Code Here*/
-   //*source_file_name=name;
-     file=fopen(name,"r");
-   
-
+    file=fopen(name,"r"); //opens the file in read only mode
     if(file==NULL){ perror ("File loading returned NULL, did you specify a file"); }
-    
+    //the above line checks for a NULL file. Prints the error if there is no argument
 
-   strcpy(source_file_name,name);
-   dte=ctime(&timer);
-       
+    strcpy(source_file_name,name); //copies the argv to the source_file array
+    dte=ctime(&timer); 
+     
 
 
     return file;
@@ -54,16 +51,24 @@ BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
     static int line_number = 0;
     int eofresult=1;
 
-    eofresult= feof(src_file);
+    eofresult= feof(src_file); //checks for the end of file and saves result
 
-    if (eofresult==0)
+    if (eofresult==0) //if it's not the end of the file continue
     {
-     fgets(source_buffer,80,src_file);	/*  Missing Code Here */
-     puts(source_buffer);
+     line_number++;
+     fgets(source_buffer,80,src_file);
+     
+     //next 2 lines create the print buffer from the linenumber and sourcebuffer
+
+     sprintf(print_buffer,"   %d:",line_number);
+     sprintf(print_buffer+strlen(print_buffer),source_buffer);
+     
+     print_line(print_buffer,src_name,todays_date);
+     
 
    return (TRUE);
     }
-    else
+    else //if it's the end of the file return false and will cause program exit
     {
         return (FALSE);
     }
