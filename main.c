1e751c222a619c26fb4abce68fba8a25db1e8d12
atomhax <adammiller7@gmail.com>
2014-02-23 14:24:13 -0700

    Changed main.c
    1)init_lister will now open the file
    
    2)File name can now be passed from-
    command line
    
    3) added files a.out and test which can be ignored for now

--- main.c
+++ main.c
@@ -1,22 +1,39 @@
 #include "common.h"
 
+
 int main (int argc, const char *argv[])
 {
     FILE *source_file;
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
-    
-   /* Missing Code Here */
-   source_file= fopen("test","r");
+    char firstline[80];
+
+   /* Missing Code Here */ 
    
-    return 0;
+   //source_file=fopen(argv[1],"r");
+   //fgets(firstline,80,source_file);
+   //puts(firstline);
+
+   source_file=init_lister(argv[1],source_name,date);
+      
+
+
+return 0;
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
 {
     time_t timer;
+
     FILE *file;
     
-    /* Missing Code Here */
+    /* Missing Code Here*/
+   //*source_file_name=name;
+   strcpy(source_file_name,name);
+    file=fopen(name,"r");
+   // time(&timer);
+   // *dte=timer;    
+
+
     return file;
 }
 BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
