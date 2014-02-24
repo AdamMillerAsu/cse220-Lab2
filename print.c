902b9da9dee2e3a89a3f613ecff0926807bd7407
dpwong <daniel.p.wong2@gmail.com>
2014-02-23 19:50:16 -0700

    Update print.c

--- print.c
+++ print.c
@@ -8,12 +8,12 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
     
-    if (++line_count > MAX_LINES_PER_PAGE)
+    if (++line_count > MAX_LINES_PER_PAGE) //checks for new page
     {
         print_page_header(source_name_to_print, date_to_print);
         line_count = 1;
     }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
+    if (strlen(line) > MAX_PRINT_LINE_LENGTH) //truncates line if needed
     {
         line[81] = '\0';
     }
